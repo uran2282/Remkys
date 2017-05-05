bf8335443ad2f6e9028a321d3bf4d237f9c13de1
Евгений Жураховский <nuklid2@gmail.com>
2017-05-05 11:48:24 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+﻿
+
+
+
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        if(!$('.menu').hasClass('open')){
+            $('.menu').addClass('open');
+            return false;
+        }
+    });
+
+    $(document).click(function(event) {
+        if($('.menu').hasClass('open')){
+            if ($(event.target).closest(".menu").length){return;} 
+            $('.menu').removeClass('open');
+            return false;
+        }
+    });
+
+    $(document).scroll(function(event) {
+        $('.menu').removeClass('open');
+    });
+
+
+    $( ".alert_btn" ).click(function() {
+      alert_function();
+    });
+    function alert_function(){ 
+        $( ".alert_fixed" ).addClass( "hide" );
+    }
+});
+
+
+
